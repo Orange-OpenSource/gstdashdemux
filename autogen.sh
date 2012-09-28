b860208128e5292b8783502e3d14d8e993d0db38
David Corvoysier <david.corvoysier@orange.com>
2012-09-28 12:53:20 +0200

    Modified autogen to create missing files to fix autotools errors

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,14 @@
 # you can either set the environment variables AUTOCONF, AUTOHEADER, AUTOMAKE,
 # ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
 # unset and get the defaults
+if [ ! -f NEWS ]
+then
+    touch NEWS
+fi
+if [ ! -f ChangeLog ]
+then
+    touch ChangeLog
+fi
 
 autoreconf --verbose --force --install --make $1 || {
  echo 'autogen.sh failed';
